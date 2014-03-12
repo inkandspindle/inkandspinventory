97335b5a27052c4d1d2fcd7eab4be442d59120f5
Dave Slutzkin <daveslutzkin@fastmail.fm>
2014-03-12 18:56:17 +1100

    Empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inkandspinventory::Application.config.secret_key_base = '7852d1c7057c4c9e61abc49f8d487286c08142a1d5664d4b38867d0309234b3d57b2d1c9479771d028fac1a2fbf8ad6931d3fa4b651c6b46816fe09a53ceb99c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inkandspinventory::Application.config.session_store :cookie_store, key: '_inkandspinventory_session'
